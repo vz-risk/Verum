24bdfa50776a86ac2a94c73fec1c5eb1c99c01d4
Gabriel Bassett <gabe@infosecanalytics.com>
2014-11-20 21:18:20 -0600

    Worked around issues with neo4j subgraph query.   Both neo4j merge and query should work and benefit from batch processing (in and out of graph database).  Note:  TitanDB query and merge scripts do not yet use batch operations and so are very inefficient.

--- verum/cg_query.py
+++ verum/cg_query.py
@@ -253,52 +253,62 @@ def get_neo_subgraph(topic, max_depth=4, neo_conf="http://localhost:7474/db/data
     # Get IDs of topic nodes in graph (if they exist).  Also add topics to subgraph
     topic_ids = set()
     for t, data in topic.nodes(data=True):
-        cypher = ("MATCH (topic: {0}) "
-                  "WHERE topic.key = {1} AND topic.value = {2} "
-                  "RETURN topic as topics").format(data['class'], "{KEY}", "{VALUE}")
+        cypher = ("MATCH (topic: {0} {1}) "
+                  "RETURN collect(topic) as topics").format(data['class'], "{key:{KEY}, value:{VALUE}}")
         props = {"KEY":data['key'], "VALUE":data['value']}
-        for record in neo_graph.cypher.execute(cypher, props):
-            print record  # DEBUG
+        records = neo_graph.cypher.execute(cypher, props)
+        #print type(records)
+        for record in records:
+            #print record  # DEBUG
             for topic in record.topics:
                 attr = dict(topic.properties)
                 uri = u'class={0}&key={1}&value={2}'.format(attr['class'],attr['key'], attr['value'])
                 sg.add_node(uri, attr)
-                topic_ids.add(topic._Node__id)
+                topic_ids.add(topic.ref.split("/")[-1])
 
     # Add nodes at depth 1  (done separately as it doesn't include the intermediary
+    nodes = dict()
     if max_depth > 0:
         if max_depth == 1:
             cypher = ("path=MATCH (topic)-[rel:describedBy|influences]-(node: attribute)"
                       "WHERE id(topic) IN {TOPICS}"
-                      "RETURN DISTINCT(r IN rels(path) | r) as rels")
+                      "RETURN DISTINCT extract(r IN rels(path) | r) as rels, extract(n IN nodes(path) | n) as nodes ")
             attr = {"TOPICS":list(topic_ids)}
         else:
             cypher = ("MATCH path=(topic: attribute)-[rel:describedBy|influences]-(node: attribute) "
                       "WHERE id(topic) IN {TOPICS} "
-                      "RETURN DISTINCT extract(r IN rels(path) | r) as rels "
+                      "RETURN DISTINCT extract(r IN rels(path) | r) as rels, extract(n IN nodes(path) | n) as nodes "
                       "UNION "
                       "MATCH path=(topic: attribute)-[rel1: describedBy|influences]-(intermediate: attribute)-[rel2: describedBy|influences]-(node: attribute) "
-                      "WHERE id(topic) IN {TOPIC} AND NOT intermediate.key in {DONT_FOLLOW} and length(path) <= {MAX_DEPTH} "
-                      "RETURN DISTINCT extract(r IN rels(path) | r) as rels")
+                      "WHERE id(topic) IN {TOPICS} AND NOT intermediate.key in {DONT_FOLLOW} and length(path) <= {MAX_DEPTH} "
+                      "RETURN DISTINCT extract(r IN rels(path) | r) as rels, extract(n IN nodes(path) | n) as nodes ")
             attr = {"MAX_DEPTH": max_depth,
-                    "TOPIC": list(topic_ids),
+                    "TOPICS": list(topic_ids),
                     "DONT_FOLLOW": ['enrichment', 'classification']}
-
-        for record in neo_graph.cypher.stream(cypher, attr):
+        # for record in neo_graph.cypher.stream(cypher, attr):  # TODO: Would prefer streaming but it errore
+        for record in neo_graph.cypher.execute(cypher, attr):
+            for node in record.nodes:
+                attr = dict(node.properties)
+                uri = 'class={0}&key={1}&value={2}'.format(attr['class'],attr['key'], attr['value'])
+                sg.add_node(uri, attr)
+                nodes[node.ref.split("/")[-1]] = uri
             for rel in record.rels:
+#                print type(rel) # DEBUG
                 # add edges SRC node
-                src_attr = dict(rel.start_node.properties)
-                src_uri = u"class={0}&key={1}&value={2}".format(src_attr['class'], src_attr['key'], src_attr['value'])
-                sg.add_node(src_uri, src_attr)
+#                src_attr = dict(rel.start_node.properties)
+#                src_uri = u"class={0}&key={1}&value={2}".format(src_attr['class'], src_attr['key'], src_attr['value'])
+#                sg.add_node(src_uri, src_attr)
+                src_uri = nodes[rel.start_node.ref.split("/")[-1]]  # src node uri from neo4j ID
 
                 # Add edge DST node
-                dst_attr = dict(rel.end_node.properties)
-                dst_uri = u"class={0}&key={1}&value={2}".format(dst_attr['class'], dst_attr['key'], dst_attr['value'])
-                sg.add_node(dst_uri, dst_attr)
+#                dst_attr = dict(rel.end_node.properties)
+#                dst_uri = u"class={0}&key={1}&value={2}".format(dst_attr['class'], dst_attr['key'], dst_attr['value'])
+#                sg.add_node(dst_uri, dst_attr)
+                dst_uri = nodes[rel.end_node.ref.split("/")[-1]]  # dst node uri from neo4j ID
 
                 # add edge
-                edge_attr = dict(record.rels.properties)
-                edge_attr['relationship'] = record.rels.type
+                edge_attr = dict(rel.properties)
+                edge_attr['relationship'] = rel.type
                 source_hash = uuid.uuid3(uuid.NAMESPACE_URL, src_uri)
                 dest_hash = uuid.uuid3(uuid.NAMESPACE_URL, dst_uri)
                 edge_uri = u"source={0}&destionation={1}".format(str(source_hash), str(dest_hash))
