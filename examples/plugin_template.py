ea41578f0f7778c18adf7a93396de4ec482c1830
Gabriel Bassett <gabe@infosecanalytics.com>
2015-06-30 16:39:05 -0500

    significantly revised the template, including adding minion documentation.

--- examples/plugin_template.py
+++ examples/plugin_template.py
@@ -51,14 +51,14 @@ from datetime import datetime # timedelta imported above
 import uuid
 import ConfigParser
 import inspect
+"""
 try:
-    import tldextract
+    import <SOME UNIQUE MODULE>
     module_import_success = True
 except:
     module_import_success = False
-    logging.error("Module import failed.  Please install the following module: tldextract.")
-    raise
-
+    logging.error("Module import failed.  Please install the following module: <SOME UNIQUE MODULE>.")
+"""
 
 ## SETUP
 loc = inspect.getfile(inspect.currentframe())
@@ -90,14 +90,16 @@ class PluginOne(IPlugin):
     #  CHANGEME: enrichment [type, successful_load, name, description, inputs to enrichment such as 'ip', cost, speed]
     #  CHANGEME: interface [type, successful_load, name]
     #  CHANGEME: score [type, successful_load, name, description, cost, speed]
-    #  CHANGEME: minion [TBD]
+    #  CHANGEME: minion [type, successful_load, name, description, cost]
     def configure(self):
         """
 
-        :return: return list of [configure success (bool), name, description, list of acceptable inputs, resource cost (1-10, 1=low), speed (1-10, 1=fast)]
+        :return: return list of configuration variables starting with [plugin_type, successful_load, name, description, <PLUGIN TYPE SPECIFIC VALUES>]
         """
         config_options = config.options("Configuration")
 
+        # Cost and speed are not applicable to all plugin types
+        """
         if 'cost' in config_options:
             cost = config.get('Configuration', 'cost')
         else:
@@ -106,6 +108,7 @@ class PluginOne(IPlugin):
             speed = config.get('Configuration', 'speed')
         else:
             speed = 9999
+        """
 
         if config.has_section('Documentation') and 'description' in config.options('Documentation'):
             description = config.get('Configuration', 'type')
@@ -119,26 +122,33 @@ class PluginOne(IPlugin):
             logging.error("'Type' not specified in config file.")
             return [None, False, NAME, description, None, cost, speed]
 
+        # Inputs is only applicable to enrichment plugins
+        """
         if 'inputs' in config_options:
             self.inputs = config.get('Configuration', 'Inputs')
             self.inputs = [l.strip().lower() for l in self.inputs.split(",")]
         else:
             logging.error("No input types specified in config file.")
             return [plugin_type, False, NAME, description, None, cost, speed]
+        """
 
+        # Module success is only applicable to plugins which import unique code
+        """
         if not module_import_success:
             logging.error("Module import failure caused configuration failure.")
             return [plugin_type, False, NAME, description, self.inputs, cost, speed]
-        else:
-            return [plugin_type, True, NAME, description, self.inputs, cost, speed]
+        """
+
+        return [plugin_type, True, NAME, description, self.inputs, cost, speed]
 
 
+    ############  GENERAL NOTES ############
+    #  CHANGEME: All functions must implement a "configuration()" function
     #  CHANGEME: The correct type of execution function must be defined for the type of plugin
+    ############  GENERAL NOTES ############
+
+
     #  CHANGEME: enrichment: "run(<thing to enrich>, inputs, start_time, any other plugin-specific attributes-MUST HAVE DEFAULTS)
-    #  CHANGEME: interface: enrich(graph, any other plugin-specific attributes-MUST HAVE DEFAULTS)
-    #  CHANGEME:            query(topic, max_depth, config, dont_follow, any other plugin-specific attributes-MUST HAVE DEFAULTS)
-    #  CHANGEME: score: score(subgraph, topic, any other plugin-specific attributes-MUST HAVE DEFAULTS)
-    #  CHANGEME: minion [TBD] 
     #  CHANGEME: Enrichment plugin specifics:
     #  -     Created nodes/edges must follow http://blog.infosecanalytics.com/2014/11/cyber-attack-graph-schema-cags-20.html
     #  -     The enrichment should include a node for the <thing to enrich>
@@ -147,6 +157,21 @@ class PluginOne(IPlugin):
     #  -     Each enrichment datum should have a node
     #  -     An edge should exist from <thing to enrich> to each enrichment datum
     #  -     The run function should then return a networkx directed multi-graph including the nodes and edges
+    def run(self, enrichment_target, inputs=None, start_time=""):
+        """
+
+        :param enrichment_target: a string containing a target to enrich
+        :return: a networkx graph representing the sections of the domain
+        """
+
+
+        pass  # TODO: Place enrichment in here
+
+        return g
+
+
+    #  CHANGEME: interface: enrich(graph, any other plugin-specific attributes-MUST HAVE DEFAULTS)
+    #  CHANGEME:            query(topic, max_depth, config, dont_follow, any other plugin-specific attributes-MUST HAVE DEFAULTS)
     #  CHANGEME: Interface plugin specifics:
     #  -     In the most efficient way possible, merge nodes and edges into the storage medium
     #  -     Merger of nodes should be done based on matching key & value.
@@ -160,253 +185,62 @@ class PluginOne(IPlugin):
     #  -     edges within the max_distance from any node in the topic graph from the storage backend graph.
     #  -     As a default, ['enrichment', 'classification'] should not be followed.
     #  -     The query function must add a 'topic_distance' property to all nodes.
-    #  CHANGEME: Score plugin specifics:
-    #  -     Scoring plugins should take a topic and networkx (sub)graph and return a dictionary keyed with the node (name) and with
-    #  -     values of the score assigned to the node for the given topic.
-    #  CHANGEME: Minion plugin specifics:
-    #  -     [TBD]
-    def run(self, domain, inputs=None, start_time="", include_subdomain=False):
-        """
-
-        :param domain: a string containing a domain to look up
-        :param include_subdomain: Boolean value.  Default False.  If true, subdomain will be returned in enrichment graph
-        :return: a networkx graph representing the sections of the domain
-        """
-        if inputs is None:
-            inputs = self.inputs
-
-        if type(start_time) is str:
-            try:
-                time = datetime.strptime("%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%dT%H:%M:%SZ")
-            except:
-                time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-        elif type(star_time) is datetime:
-            time = start_time.strftime("%Y-%m-%dT%H:%M:%SZ")
-        else:
-            time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-
-        ext = tldextract.extract(domain)
-        now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-        g = nx.MultiDiGraph()
-
-        # Get or create Domain node
-        domain_uri = "class=attribute&key={0}&value={1}".format("domain", domain)
-        g.add_node(domain_uri, {
-            'class': 'attribute',
-            'key': "domain",
-            "value": domain,
-            "start_time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),  # graphml does not support 'none'
-            "uri": domain_uri
-        })
-
-        # Get or create Enrichment node
-        tld_extract_uri = "class=attribute&key={0}&value={1}".format("enrichment", "tld_extract")
-        g.add_node(tld_extract_uri, {
-            'class': 'attribute',
-            'key': "enrichment",
-            "value": "tld_extract",
-            "start_time": now,
-            "uri": tld_extract_uri
-        })
-
-        # Get or create TLD node
-        tld_uri = "class=attribute&key={0}&value={1}".format("domain", ext.suffix)
-        g.add_node(tld_uri, {
-            'class': 'attribute',
-            'key': "domain",
-            "value": ext.suffix,
-            "start_time": now,
-            "uri": tld_uri
-        })
-
-        # Link domain to tld
-        edge_attr = {
-            "relationship": "describedBy",
-            "start_time": now,
-            "origin": "tld_extract",
-            "describedBy":"suffix"
-        }
-        source_hash = uuid.uuid3(uuid.NAMESPACE_URL, domain_uri)
-        dest_hash = uuid.uuid3(uuid.NAMESPACE_URL, tld_uri)
-        edge_uri = "source={0}&destionation={1}".format(str(source_hash), str(dest_hash))
-        rel_chain = "relationship"
-        while rel_chain in edge_attr:
-            edge_uri = edge_uri + "&{0}={1}".format(rel_chain,edge_attr[rel_chain])
-            rel_chain = edge_attr[rel_chain]
-        if "origin" in edge_attr:
-            edge_uri += "&{0}={1}".format("origin", edge_attr["origin"])
-        edge_attr["uri"] = edge_uri
-        g.add_edge(domain_uri, tld_uri, edge_uri, edge_attr)
-
-
-        # Get or create mid domain node
-        mid_domain_uri = "class=attribute&key={0}&value={1}".format("domain", ext.domain)
-        g.add_node(mid_domain_uri, {
-            'class': 'attribute',
-            'key': "domain",
-            "value": ext.domain,
-            "start_time": now,
-            "uri": mid_domain_uri
-        })
-
-        # Link domain to mid_domain
-        edge_attr = {
-            "relationship": "describedBy",
-            "start_time": now,
-            "origin": "tld_extract",
-            "describedBy":"domain"
-        }
-        source_hash = uuid.uuid3(uuid.NAMESPACE_URL, domain_uri)
-        dest_hash = uuid.uuid3(uuid.NAMESPACE_URL, mid_domain_uri)
-        edge_uri = "source={0}&destionation={1}".format(str(source_hash), str(dest_hash))
-        rel_chain = "relationship"
-        while rel_chain in edge_attr:
-            edge_uri = edge_uri + "&{0}={1}".format(rel_chain,edge_attr[rel_chain])
-            rel_chain = edge_attr[rel_chain]
-        if "origin" in edge_attr:
-            edge_uri += "&{0}={1}".format("origin", edge_attr["origin"])
-        edge_attr["uri"] = edge_uri
-        g.add_edge(domain_uri, mid_domain_uri, edge_uri, edge_attr)
-
-
-        # if including subdomains, create subdomain and node
-        if include_subdomain:
-            # Get or create mid domain node
-            subdomain_uri = "class=attribute&key={0}&value={1}".format("domain", ext.subdomain)
-            g.add_node(subdomain_uri, {
-                'class': 'attribute',
-                'key': "domain",
-                "value": ext.domain,
-                "start_time": now,
-                "uri": subdomain_uri
-            })
-
-            # Link domain to mid_domain
-            edge_attr = {
-                "relationship": "describedBy",
-                "start_time": now,
-                "origin": "tld_extract",
-                "describedBy":"subdomain"
-            }
-            source_hash = uuid.uuid3(uuid.NAMESPACE_URL, domain_uri)
-            dest_hash = uuid.uuid3(uuid.NAMESPACE_URL, subdomain_uri)
-            edge_uri = "source={0}&destionation={1}".format(str(source_hash), str(dest_hash))
-            rel_chain = "relationship"
-            while rel_chain in edge_attr:
-                edge_uri = edge_uri + "&{0}={1}".format(rel_chain,edge_attr[rel_chain])
-                rel_chain = edge_attr[rel_chain]
-            if "origin" in edge_attr:
-                edge_uri += "&{0}={1}".format("origin", edge_attr["origin"])
-            edge_attr["uri"] = edge_uri
-            g.add_edge(domain_uri, subdomain_uri, edge_uri, edge_attr)
-
-        # Link domain to enrichment
-        edge_attr = {
-            "relationship": "describedBy",
-            "start_time": now,
-            "origin": "tld_extract"
-        }
-        source_hash = uuid.uuid3(uuid.NAMESPACE_URL, domain_uri)
-        dest_hash = uuid.uuid3(uuid.NAMESPACE_URL, tld_extract_uri)
-        edge_uri = "source={0}&destionation={1}".format(str(source_hash), str(dest_hash))
-        rel_chain = "relationship"
-        while rel_chain in edge_attr:
-            edge_uri = edge_uri + "&{0}={1}".format(rel_chain,edge_attr[rel_chain])
-            rel_chain = edge_attr[rel_chain]
-        if "origin" in edge_attr:
-            edge_uri += "&{0}={1}".format("origin", edge_attr["origin"])
-        edge_attr["uri"] = edge_uri
-        g.add_edge(domain_uri, tld_extract_uri, edge_uri, edge_attr)
-
-        return g
-
-
-   def enrich(self, g):  # Neo4j
+   def enrich(self, g):
         """
 
         :param g: networkx graph to be merged
-        :param neo4j: bulbs neo4j config
         :return: Nonetype
-
-        Note: Neo4j operates differently from the current titan import.  The neo4j import does not aggregate edges which
-               means they must be handled at query time.  The current titan algorithm aggregates edges based on time on
-               merge.
         """
-        #neo4j_graph = NEO_Graph(neo4j)  # Bulbs
-        neo_graph = py2neoGraph(self.neo4j_config)
-        nodes = set()
-        node_map = dict()
-        edges = set()
-        settled = set()
-        # Merge all nodes first
-        tx = neo_graph.cypher.begin()
-        cypher = ("MERGE (node: {0} {1}) "
-                  "ON CREATE SET node = {2} "
-                  "RETURN collect(node) as nodes"
-                 )
-        # create transaction for all nodes
-        for node, data in g.nodes(data=True):
-            query = cypher.format(data['class'], "{key:{KEY}, value:{VALUE}}", "{MAP}")
-            props = {"KEY": data['key'], "VALUE":data['value'], "MAP": data}
-            # TODO: set "start_time" and "finish_time" to dummy variables in attr.
-            # TODO:  Add nodes to graph, and cyper/gremlin query to compare to node start_time & end_time to dummy
-            # TODO:  variable update if node start > dummy start & node finish < dummy finish, and delete dummy
-            # TODO:  variables.
-            tx.append(query, props)
-        # commit transaction and create mapping of returned nodes to URIs for edge creation
-        for record_list in tx.commit():
-            for record in record_list:
-    #            print record, record.nodes[0]._Node__id, len(record.nodes)
-                for n in record.nodes:
-    #                print n._Node__id
-                    attr = n.properties
-                    uri = "class={0}&key={1}&value={2}".format(attr['class'], attr['key'], attr['value'])
-                    node_map[uri] = int(n.ref.split("/")[1])
-    #                node_map[uri] = n._Node__id
-    #    print node_map  # DEBUG
+        pass  # TODO: Replace this with storage into a backend storage system
 
-        # Create edges
-        cypher = ("MATCH (src: {0}), (dst: {1}) "
-                  "WHERE id(src) = {2} AND id(dst) = {3} "
-                  "CREATE (src)-[rel: {4} {5}]->(dst) "
-                 )
-        tx = neo_graph.cypher.begin()
-        for edge in g.edges(data=True):
-            try:
-                if 'relationship' in edge[2]:
-                    relationship = edge[2].pop('relationship')
-                else:
-                    # default to 'described_by'
-                    relationship = 'describedBy'
 
-                query = cypher.format(g.node[edge[0]]['class'],
-                                      g.node[edge[1]]['class'],
-                                     "{SRC_ID}",
-                                     "{DST_ID}",
-                                      relationship,
-                                      "{MAP}"
-                                     )
-                props = {
-                    "SRC_ID": node_map[edge[0]],
-                    "DST_ID": node_map[edge[1]],
-                    "MAP": edge[2]
-                }
+    #  CHANGEME: score: score(subgraph, topic, any other plugin-specific attributes-MUST HAVE DEFAULTS)
+    #  CHANGEME: Score plugin specifics:
+    #  -     Scoring plugins should take a topic and networkx (sub)graph and return a dictionary keyed with the node (name) and with
+    #  -     values of the score assigned to the node for the given topic.
+    def score(self, sg, topic):  # get_bayesian_network_probability
+        """
 
-                # create the edge
-                # NOTE: No attempt is made to deduplicate edges between the graph to be merged and the destination graph.
-                #        The query scripts should handle this.
-        #        print edge, query, props  # DEBUG
-                tx.append(query, props)
-        #        rel = py2neoRelationship(node_map[src_uri], relationship, node_map[dst_uri])
-        #        rel.properties.update(edge[2])
-        #        neo_graph.create(rel)  # Debug
-        #        edges.add(rel)
-            except:
-                print edge
-                print node_map
-                raise
+        :param sg: egocentric subgraph around topic in networkx format
+        :param topic: graph of topics
+        :return: Dictionary of probabilities keyed by node
+        """
+        scores = dict() 
 
-        # create edges all at once
-        #print edges  # Debug
-    #    neo_graph.create(*edges)
-        tx.commit()
+        pass  # TODO: Replace with code to score the subgraph with respect to the topic
+
+        return scores
+
+
+
+    #  CHANGEME: minion: minion() 
+    #  CHANGEME:        start() 
+    #  CHANGEME:        stop()
+    #  CHANGEME:        isAlive()
+    #  CHANGEME: Minion plugin specifics:
+    #  -     Minions fit exist in a separate directory to prevent them importing themselves when they import their own VERUM instance
+    #  -     The minion configuration function must take an argument of the parent verum object.  When not present, it shouldn't error but
+    #  -      instead return with successful_load set to false and a logging.info message that the parent was not passed in.
+    #  -     Must have 4 functions: minion(), start(), and stop() and isAlive()
+    #  -     minion() is the function which will be threaded.  **Make sure to call create the new verum instance WITHIN this function
+    #  -      to avoid SQLite errors!**
+    #  -     start() creates the thread object as an attribute of the plugin class and starts it
+    #  -     stop() stops the thread.  Preferably with both a normal exit by setting a shutdown variable of the plugin class as well as a 
+    #  -      force stop option which removes the thread object
+    #  -     isAlive() calls the thread isAlive() function and returns the status
+    def minion(self,  *args, **xargs):
+        pass  # TODO: Write the function which will be threaded to form the minion
+
+    def start(self, *args, **xargs):
+        self.thread = threading.Thread(target=self.minion, *args, **xargs)
+        self.thread.start()
+
+    def isAlive(self):
+        return self.thread.isAlive()
+
+    def stop(self, force=True):
+        if force:
+            self.thread = None  # zero out thread
+        else:
+            self.shutdown = False  # just dont' iterate.  May take up to (SLEEP_TIME) hours
+
